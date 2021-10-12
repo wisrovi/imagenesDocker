cc38ce42570d2dd093775b9ad883c8dc665cd689
William Rodriguez <wisrovi.rodriguez@gmail.com>
2021-10-12 10:37:23 -0500

    Add files via upload

--- kafka y zookeeper/kafka-python-code/data_generator.py
+++ kafka y zookeeper/kafka-python-code/data_generator.py
@@ -1,11 +1,13 @@
 import random 
 import string 
+from datetime import datetime
 
 user_ids = list(range(1, 101))
 recipient_ids = list(range(1, 101))
 
-def generate_message(message = None) -> dict:
-    random_user_id = random.choice(user_ids)
+def generate_message(message = None, random_user_id=None) -> dict:
+    if random_user_id is None:
+        random_user_id = random.choice(user_ids)
     # Copy the recipients array
     recipient_ids_copy = recipient_ids.copy()
     # User can't send message to himself
@@ -18,4 +20,15 @@ def generate_message(message = None) -> dict:
             'user_id': random_user_id,
             'recipient_id': random_recipient_id,
             'message': message
+        }
+
+def design_message(message, user, date=None, grupo=None):
+    if date is None:
+        date = datetime.now()
+        date = str(date)
+    return {
+            'user_id': user,
+            'date': date,
+            'message': message,
+            'chat_id':grupo
         }
