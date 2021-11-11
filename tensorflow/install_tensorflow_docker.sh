a7c6f396ade5b93ec4152eb65ec114c5f683c746
William Rodriguez <wisrovi.rodriguez@gmail.com>
2021-11-11 12:16:13 -0500

    Update install_tensorflow_docker.sh

--- tensorflow/install_tensorflow_docker.sh
+++ tensorflow/install_tensorflow_docker.sh
@@ -15,6 +15,7 @@ sudo docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -
 
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo apt install docker-compose
 
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
