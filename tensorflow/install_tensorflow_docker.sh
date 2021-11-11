dde54c0b8e352c4456b79e7973894c1a5c717301
William Rodriguez <wisrovi.rodriguez@gmail.com>
2021-11-11 12:08:24 -0500

    Update install_tensorflow_docker.sh

--- tensorflow/install_tensorflow_docker.sh
+++ tensorflow/install_tensorflow_docker.sh
@@ -8,9 +8,9 @@ sudo ubuntu-drivers autoinstall
 
 curl -sSL https://get.docker.com | sh
 sudo usermod -a -G docker $USER
-docker version
+sudo docker version
 
-docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+sudo docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
 
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
@@ -22,14 +22,14 @@ sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
 sudo systemctl restart docker
 
 
-docker run --gpus all --name=NVIDIA nvidia/cuda:9.0-base nvidia-smi
+sudo docker run --gpus all --name=NVIDIA nvidia/cuda:9.0-base nvidia-smi
 
-docker pull tensorflow/tensorflow:latest-gpu
-docker pull tensorflow/tensorflow:latest-gpu-jupyter
+sudo docker pull tensorflow/tensorflow:latest-gpu
+sudo docker pull tensorflow/tensorflow:latest-gpu-jupyter
 
-docker run -u $(id -u):$(id -g) --gpus all -d --name tensorflow -v /home/rh/Documentos/docker/tensorflow:/tf -p 8888:8888 -p 6006:6006 tensorflow/tensorflow:latest-gpu-jupyter
+sudo docker run -u $(id -u):$(id -g) --gpus all -d --name tensorflow -v /home/rh/Documentos/docker/tensorflow:/tf -p 8888:8888 -p 6006:6006 tensorflow/tensorflow:latest-gpu-jupyter
 
-docker pull tensorflow/tensorflow:latest-gpu
-docker pull tensorflow/tensorflow:latest-gpu-jupyter
+sudo docker pull tensorflow/tensorflow:latest-gpu
+sudo docker pull tensorflow/tensorflow:latest-gpu-jupyter
 
 sudo reboot
