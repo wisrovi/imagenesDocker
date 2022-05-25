9f23cb78741c2bb2bdbfa21b45749b76979f3bc4
William Rodriguez <wisrovi.rodriguez@gmail.com>
2022-05-25 15:44:48 -0500

    poniendo https con certbot en wordpress

---
+++ hobbies/wordpress/nginx_wordpress_mariadb/config/ssl/generar.sh
@@ -0,0 +1,5 @@
+openssl req -x509 -outform pem -out chain.pem -keyout privkey.pem \
+  -newkey rsa:4096 -nodes -sha256 -days 3650 \
+  -subj '/CN=localhost' -extensions EXT -config <( \
+   printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+cat chain.pem > fullchain.pem
