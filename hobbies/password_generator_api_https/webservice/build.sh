8eb2398c177a9d684b4ede653fb8956112c86dc6
William Rodriguez <wisrovi.rodriguez@gmail.com>
2022-05-27 15:59:35 -0500

    api en https para generar password y respaldo en mongoDB

--- hobbies/password_generator_api_https/webservice/build.sh
+++
@@ -1,2 +0,0 @@
-sudo docker build -t password_generator:V1.0 .
-sudo docker-compose up -d
--- hobbies/password_generator_api_https/webservice/src/local_aplication/example.py
+++
@@ -1,6 +0,0 @@
-from local_aplication.libraries.Mongo_Password_Generator import Buscar_generar_clave, Users
-
-USER = "wisrovi_3"
-ORIGIN = Users.api
-rta = Buscar_generar_clave(USER, ORIGIN)
-print(rta)
