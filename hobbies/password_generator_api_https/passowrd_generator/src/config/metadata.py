236f3893345a3320d89abed2e6d19ca1138ac4d5
William Rodriguez <wisrovi.rodriguez@gmail.com>
2022-05-27 10:18:35 -0500

    instanciando carpetas para organizar los archivos de la api

---
+++ hobbies/password_generator_api_https/passowrd_generator/src/config/metadata.py
@@ -0,0 +1,18 @@
+description = """
+Evaluar_coordenadas_correctas API ayuda a encontrar intercepciones de coordenadas en zonas, con las coordenadas recibidas y todas las coordenadas de zonas existentes. 🚀
+
+## evaluar_coordenadas_correctas
+
+Hace la comparacion con las zonas y brinda una recomendacion de coordenadas, en caso de solapamiento, para evitar que exista ese solapamiento
+
+"""
+
+titulo="Evaluar_coordenadas_correctas"
+
+version="0.0.1"
+
+contact={
+        "name": "William Rodriguez (WISROVI)",
+        "url": "https://www.linkedin.com/in/wisrovi-rodriguez/",
+        "email": "wisrovi.rodriguez@gmail.com",
+    }
