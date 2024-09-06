850fbe816060f2474be099f397e08646db416fac
William Rodriguez <wrodriguez@ecapturedtech.com>
2024-09-06 10:31:35 +0200

    [FEATURE] Update producer for permit to send images

--- devOps/colas mensajeria/kafka y zookeeper/simple-demo/producer.py
+++ devOps/colas mensajeria/kafka y zookeeper/simple-demo/producer.py
@@ -4,6 +4,7 @@ kafka-python
 aiokafka
 """
 import json
+import pandas as pd
 from confluent_kafka import Producer
 # Messages will be serialized as JSON
 def serializer(message):
@@ -12,7 +13,17 @@ def serializer(message):
 producer = Producer(
     {"bootstrap.servers": "192.168.1.60:9092"},
 )
-if _name_ == "_main_":
+if __name__ == "_main_":
+    
+    df = pd.read_csv('tu_archivo.csv')
+    for index, row in df.iterrows():
+        row_json = row.to_json()   # {'fecha': 'queso', 'tf': 10, 'pt': 100}
+        
+    producer.flush()
+    
+    
+    
+    
     dummy_message = {"nombre": "queso"}
     producer.produce(
         "messages",
