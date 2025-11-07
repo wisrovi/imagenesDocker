24ac4a963b45acc3ba96367e5f9b81c9aaadf149
williamrv-edt <138199553+williamrv-edt@users.noreply.github.com>
2025-11-07 15:09:35 +0100

    Ubuntu some worker with dispacher

--- 02 devops/Advanced docker/Diun (docker in docker)/basic/ubuntu/some_container/docker/scripts/dispatch.sh
+++ 02 devops/Advanced docker/Diun (docker in docker)/basic/ubuntu/some_container/docker/scripts/dispatch.sh
@@ -6,10 +6,10 @@ if [[ $replica_num =~ ^[0-9]+$ ]] && [ $replica_num -ge 1 ]; then
 
   IP="some_container-worker-$replica_num"
   if [ -z "$SSH_ORIGINAL_COMMAND" ]; then
-    ssh -i /home/worker$replica_num/.ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@$IP -p 50422
+    ssh -T -i /home/worker$replica_num/.ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@$IP -p 50422
   else
     echo "Running: eval \"$SSH_ORIGINAL_COMMAND\"" >&2
-    ssh -i /home/worker$replica_num/.ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@$IP -p 50422 eval "$SSH_ORIGINAL_COMMAND"
+    ssh -T -i /home/worker$replica_num/.ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@$IP -p 50422 eval "$SSH_ORIGINAL_COMMAND"
   fi
 
 else
