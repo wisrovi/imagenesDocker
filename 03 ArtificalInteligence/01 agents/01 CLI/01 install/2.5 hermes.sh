9497569e6cc7ce119572eefc1fd555f8e3f72b22
williamrv-edt <138199553+williamrv-edt@users.noreply.github.com>
2026-05-26 08:30:23 +0200

    Create 2.5 hermes.sh

---
+++ 03 ArtificalInteligence/01 agents/01 CLI/01 install/2.5 hermes.sh
@@ -0,0 +1,6 @@
+sudo curl -fsSL https://raw.githubusercontent.com/NousResearch/hermes-agent/main/scripts/install.sh | bash 
+
+# Interactive setup wizard
+hermes setup
+# Or choose your model
+hermes model
