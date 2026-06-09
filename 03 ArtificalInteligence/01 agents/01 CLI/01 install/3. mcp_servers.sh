c687aae2e4f5b7418dbbefa8ee1dfb64c0897797
William Rodriguez <wisrovi.rodriguez@gmail.com>
2026-06-09 17:08:39 +0200

    Update 3. mcp_servers.sh

--- 03 ArtificalInteligence/01 agents/01 CLI/01 install/3. mcp_servers.sh
+++ 03 ArtificalInteligence/01 agents/01 CLI/01 install/3. mcp_servers.sh
@@ -1,3 +1,17 @@
+
+
+# extras
+
+## https://github.com/gemini-cli-extensions/workspace
+gemini extensions install https://github.com/gemini-cli-extensions/workspace
+
+## sonjarqube
+gemini extensions install https://github.com/SonarSource/sonarqube-agent-plugins
+
+
+
+
+
 # codegraph
 npm i -g @colbymchenry/codegraph
 codegraph install
